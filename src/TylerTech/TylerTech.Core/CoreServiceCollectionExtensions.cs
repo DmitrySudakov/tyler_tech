f7f71fbbabe3dc63b124676cc6485dcf84cdfcbc
Dmitry Sudakov <dsudakov@dstudio.software>
2024-01-14 16:22:35 +0100

    Added and attached empty EFM client

---
+++ src/TylerTech/TylerTech.Core/CoreServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TylerTech.Core.Integrations.EfmClient;
+
+namespace TylerTech.Core;
+
+public static class CoreServiceCollectionExtensions
+{
+    public static IServiceCollection AddCoreServices(this IServiceCollection services)
+    {
+        services.AddTransient<IEfmClient, EfmClient>();
+
+        return services;
+    }
+}
